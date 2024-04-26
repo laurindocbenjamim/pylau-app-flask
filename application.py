51e2ae9e2e643df253a38ca3483b66a15513fcca
adolfosucumula <rocketmc2009@gmail.com>
2024-04-27 00:22:09 +0100

    procfile

---
+++ application.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+
+
+app = Flask("Test")
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({"sms": "Ola mundo"})
+
+@app.route('/person/<name>', methods=['GET'])
+def person(name):
+    return  f"My name is {name}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
