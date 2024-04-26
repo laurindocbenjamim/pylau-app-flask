c783b1f103c0eb239729b0085e586b5e268cf5e5
adolfosucumula <rocketmc2009@gmail.com>
2024-04-26 14:18:51 +0100

    test route

--- application.py
+++ application.py
@@ -1,6 +1,12 @@
-import core
+#import core
 
-application = core.create_application()
+#application = core.create_application()
+from flask import Flask, jsonify
+
+application = Flask("Test")
+@application.route('/', methods=['GET'])
+def home():
+    return jsonify({"sms": "Ola mundo"})
 
 if __name__ == '__main__':
     application.run()
