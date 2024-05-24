8b8000148a91b3f7f597dfdebe0d38b9061e08fb
adolfosucumula <rocketmc2009@gmail.com>
2024-05-24 13:24:11 +0100

    added mobile menu

--- core/static/assets/js/index.js
+++ core/static/assets/js/index.js
@@ -4,6 +4,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   var navScroller = document.querySelector(".nav-scroller");
   var navItem = document.querySelectorAll(".nav-item");
+  
 
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     navScroller.classList.add("fixed-nav");
@@ -14,4 +15,26 @@ function scrollFunction() {
     navScroller.classList.remove("black-bg");
     navItem.classList.remove("white-text");
   }
+
+  
+}
+  
+function mobileMenu() {
+  var x = document.getElementById("mobile-button");
+  var buttonTop = document.querySelector(".button-top");
+  var navScroller = document.querySelector(".nav-scroller");
+
+  if (buttonTop.style.display === "block") {
+    buttonTop.style.display = "none";
+  } else {
+    buttonTop.style.display = "block";
+  }
+
+  if (navScroller.style.display === "block") {
+    navScroller.style.display = "none";
+    
+  } else {
+    navScroller.style.display = "block";
+  }
+ 
 }
