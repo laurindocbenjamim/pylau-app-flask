20fca9aad22aac67a980e4c067f3b174df572a58
adolfosucumula <rocketmc2009@gmail.com>
2024-05-24 01:18:45 +0100

    added new features

--- core/static/assets/js/index.js
+++ core/static/assets/js/index.js
@@ -3,13 +3,15 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   var navScroller = document.querySelector(".nav-scroller");
+  var navItem = document.querySelectorAll(".nav-item");
 
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     navScroller.classList.add("fixed-nav");
     navScroller.classList.add("black-bg");
+    navItem.classList.add("white-text");
   } else {
     navScroller.classList.remove("fixed-nav");
     navScroller.classList.remove("black-bg");
-
+    navItem.classList.remove("white-text");
   }
 }
