70cc20c5d846e0b5281b188f41d5f7db643463fb
adolfosucumula <rocketmc2009@gmail.com>
2024-05-25 13:41:11 +0100

    addedd the register form

--- core/modules/authmodule/auth.py
+++ core/modules/authmodule/auth.py
@@ -9,3 +9,7 @@ bpapp = Blueprint("Auth", __name__, url_prefix='/auth')
 @bpapp.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('auth/auth.html')
+
+@bpapp.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('auth/register.html')
