0b1d9e9da534f5fb8bf8112394e1fa9edab6ee5a
adolfosucumula <rocketmc2009@gmail.com>
2024-05-26 11:33:10 +0100

    added jwt and pyotp

--- core/static/script/form-payment.js
+++ core/static/script/form-payment.js
@@ -1,6 +1,8 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 
 const form = document.getElementById('form-register');
+const alertmessage = document.querySelector('.alert p');
+alertmessage.style.color = "red";
 
 form.addEventListener("submit", async event => {
     event.preventDefault();
@@ -28,6 +30,7 @@ form.addEventListener("submit", async event => {
     if(resData){
       console.log(resData[0].object);
       //alert(resData[0].object)
+      alertmessage.textContent = "Username or password wrong!";
       window.open(baseUrl + '/auth/login', '_self');
     }
   } catch (err) { 
