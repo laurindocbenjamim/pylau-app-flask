f677dbe96703b809c9bcbe7568941d9ec639ea63
adolfosucumula <rocketmc2009@gmail.com>
2024-06-22 14:01:09 +0100

    extract only tables

--- app/web_scrappin_module/modules/extract_contries copy.py
+++ app/web_scrappin_module/modules/extract_contries copy.py
@@ -13,96 +13,6 @@ with_length = "https://www.iban.com/dialing-codes"
 url = "https://countrycode.org/"
 
 
-def extract_countries(url):
-    count = 0
-
-    if url is None:
-        return None
-
-    df = pd.DataFrame()
-    columns = []
-    html_page = requests.get(url).text
-    data_soup = BeautifulSoup(html_page, "html.parser")
-    thead = data_soup.find_all("thead")
-    tr = data_soup.find_all("tr")
-    tbody = data_soup.find_all("tbody")
-    rows = tbody[0].find_all("tr")
-
-    if thead is not None and len(thead) > 0:
-        for th in thead[0].find_all("th"):
-            columns.append(
-                th.contents[0]
-                .replace("\n", "")
-                .replace("\r", "")
-                .replace(" ", "")
-                .replace("\t", "")
-            )
-
-        # df = pd.DataFrame(columns=columns)
-    if rows is not None and len(rows) > 0:
-        for row in rows:
-            if count < len(rows):
-                col = row.find_all("td")
-
-                if len(col) != 0:
-                    data_dict = {}
-                    try:
-                        #myList = []
-                        #for i in range(len(col)):
-                            #myList.append(col[i].contents[0])
-                        #print(myList)
-                    
-                        
-                        
-                        data_dict = {
-                            "Country": col[0].contents[0],
-                            "Country Code": col[1].contents[0],
-                            "ISO Code": col[2].contents[0],
-                            "Population": col[3].contents[0],
-                            "Area": col[4].contents[0],
-                        }
-
-                        df1 = pd.DataFrame(data_dict, index=[0])
-                        df = pd.concat([df, df1], ignore_index=True)
-                        
-
-                        """
-                        
-                        df = pd.concat(
-                            [
-                                df,
-                                pd.DataFrame(
-                                    [
-                                        [
-                                            col[0].contents[0],
-                                            col[1].contents[0],
-                                            col[2].contents[0],
-                                            col[3].contents[0],
-                                            col[4].contents[0],
-                                            col[5].contents[0],
-                                        ]
-                                    ],
-                                    columns=columns,
-                                ),
-                            ],
-                            ignore_index=True,
-                        )
-                        """
-                    except IndexError as e:
-                        print(f"IndexError: {e}")
-                    except Exception as e:
-                        print(f"Error {e}")
-                    count += 1
-                
-            else:
-                break
-
-        return df
-    else:
-        return None
-
-
-
 def extract_countries_2(url):
     count = 0
     data_dict = {}
@@ -113,54 +23,40 @@ def extract_countries_2(url):
     columns = []
     html_page = requests.get(url).text
     data_soup = BeautifulSoup(html_page, "html.parser")
+
+    extract_tables(url)
+
+    table = data_soup.find_all("table")
     thead = data_soup.find_all("thead")
     tr = data_soup.find_all("tr")
     tbody = data_soup.find_all("tbody")
     rows = tbody[0].find_all("tr")
 
-    if thead is not None and len(thead) > 0:
-        for th in thead[0].find_all("th"):
-            columns.append(
-                th.contents[0]
-                .replace("\n", "")
-                .replace("\r", "")
-                .replace(" ", "")
-                .replace("\t", "")
-            )
+    print(f"Tables: {len(table)}")
 
-        # df = pd.DataFrame(columns=columns)
-        
+   
+
+    columns = get_each_head_item(thead)
+
     if rows is not None and len(rows) > 0:
         for row in rows:
             if count < len(rows):
                 col = row.find_all("td")
 
                 if len(col) != 0:
-                    
-
-                    """
-                    if 'Antarctica' in col[0].contents[0]:
-                        print("+================ Antarctica ===============+")
-                        for a in range(len(col)):
-                            item = get_each_item(col, a)
-                            print(f"{a} - {'N/A' if item is None else item}")
-                        
-                        break
-                    """
 
                     try:
                         myList = []
                         for a in range(len(col)):
                             
                             if len(col) == len(columns):
-                                item = get_each_item(col, a)
-                                print(f"{a} - {'N/A' if item is None else item}")
+                                item = get_each_tbody_item(col, a)
+                                #print(f"{a} - {'N/A' if item is None else item}")
                                 myList.append('null' if item is None else item)
                                 data_dict[columns[a]] = 'N/A' if item is None else item
                                 
                             else:
-                                data_dict[columns[a]] = "N/A"
-                                #myList.append("N/A")
+                                data_dict[columns[a]] = "null"
                         df = pd.concat(
                                 [
                                     df,
@@ -172,48 +68,8 @@ def extract_countries_2(url):
                                     ),
                                 ],
                                 ignore_index=True,
-                            )
-
-                        if count == 20:
-                            
-                            print(df)
-                            break
-                        """    
-                        print(" ================ Data Dictionary ================")
-                        
-
-                        df1 = pd.DataFrame(data_dict, index=[0])
-                        df = pd.concat([df, df1], ignore_index=True)
-                        #print("+================ Data Frame ===============+")
-                        #print(df.head(11))
-                        for i in range(len(col)):
-                            #myList.append(col[i].contents[0])
-                            a = 1
-                        #print(myList)
-                        
-
-                        
-                        
-                        df = pd.concat(
-                            [
-                                df,
-                                pd.DataFrame(
-                                    [
-                                        [
-                                            col[0].contents[0],
-                                            col[1].contents[0],
-                                            col[2].contents[0],
-                                            col[3].contents[0],
-                                            col[4].contents[0],
-                                            col[5].contents[0],
-                                        ]
-                                    ],
-                                    columns=columns,
-                                ),
-                            ],
-                            ignore_index=True,
-                        )
-                        """
+                            )                           
+                       
                     except IndexError as e:
                         print(f"IndexError: {e}")
                     except Exception as e:
@@ -227,13 +83,41 @@ def extract_countries_2(url):
     
     print(data_dict)
     return None
+
+def extract_tables(url):
+  
+    if url is None:
+        return None
+
+    html_page = requests.get(url).text
+    data_soup = BeautifulSoup(html_page, "html.parser")
+    table = data_soup.find_all("table")
+
+    if len(table) > 0:
+        return table
     
+    return []
 
-df = extract_countries(url)
-print("\n")
-#print(df)
+def get_each_head_item(thead):
+    columns = []
+    try:
+        if thead is not None and len(thead) > 0:
+            for th in thead[0].find_all("th"):
+                columns.append(
+                    th.contents[0]
+                    .replace("\n", "")
+                    .replace("\r", "")
+                    .replace(" ", "")
+                    .replace("\t", "")
+                )
+        return columns    
+    except IndexError as e:
+        return None
+    except Exception as e:
+        return None
+    return None
 
-def get_each_item(list, index):
+def get_each_tbody_item(list, index):
     try:
         return list[index].contents[0].text
     except IndexError as e:
@@ -245,4 +129,4 @@ print("\n")
 print("Second function")
 print("\n")
 df = extract_countries_2(url)
-#print(df)
+print(df.head(20))
