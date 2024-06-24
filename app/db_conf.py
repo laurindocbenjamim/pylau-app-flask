ce18cf5fa39932dc262dbcf7d8c43782348f295c
adolfosucumula <rocketmc2009@gmail.com>
2024-06-25 00:49:22 +0100

    change db server 2

--- app/db_conf.py
+++ app/db_conf.py
@@ -37,7 +37,7 @@ def connect_to_db_server(app,type_db=None):
         heroku = 'jdbc:postgresql://ec2-52-31-2-97.eu-west-1.compute.amazonaws.com:5432/db5veivij96r5u?sslmode=require&user=fiysuzvofhprpp&password=ab6e8ad51efac658eca5c1b66056b9438d8866a522daeb3fee983b66970c0883'
         cloud_db = "postgres://fiysuzvofhprpp:ab6e8ad51efac658eca5c1b66056b9438d8866a522daeb3fee983b66970c0883@ec2-52-31-2-97.eu-west-1.compute.amazonaws.com:5432/db5veivij96r5u?sslmode=require"
         
-        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI_1
+        app.config["SQLALCHEMY_DATABASE_URI"] = cloud_db
         
     else:
         app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test3.db"
