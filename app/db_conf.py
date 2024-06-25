c2f4bac2f3af36b74675fdbc79d6db122246378e
adolfosucumula <rocketmc2009@gmail.com>
2024-06-25 01:13:29 +0100

    change db server 6

--- app/db_conf.py
+++ app/db_conf.py
@@ -13,26 +13,22 @@ db = SQLAlchemy()
 # This function is used to connect to the local database
 def connect_to_db_server(app,type_db=None):
     if type_db == 'mysql':
-        DATABASE_URI_1 = 'mysql+mysqlconnector://{user}:{password}@{host}/{database}'\
+        DATABASE_URI_1 = 'mysql+mysqlconnector://{user}:{password}@{host_name}/{database}'\
             .format(user=os.getenv('DB_USER'), password=os.getenv('DB_PASSWORD'), \
-                    host=os.getenv('DB_SERVER'), database=os.getenv('DB_NAME'))   
+                    host_name=os.getenv('DB_SERVER'), database=os.getenv('DB_NAME'))   
                 
-        DATABASE_URI_2 = 'mysql+mysqlconnector://{user}:{password}@{host}/{database}'\
+        DATABASE_URI_2 = 'mysql://{user}:{password}@{host_name}:{port}/{database}'\
             .format(user=os.getenv('DB_USER'), password=os.getenv('DB_PASSWORD'), \
-                    host=os.getenv('DB_SERVER'), database=os.getenv('DB_NAME'))
+                    host_name=os.getenv('DB_SERVER'), port='3306', database=os.getenv('DB_NAME'))
         
         DATABASE_URI_3 = 'jdbc:mysql://{host_name}:{port}/{database}?{sslmode}&{user}&{password}'\
             .format(host_name='ec2-52-31-2-97.eu-west-1.compute.amazonaws.com', port='5432', database='db5veivij96r5u',\
                     sslmode='require', user='fiysuzvofhprpp', \
                         password='ab6e8ad51efac658eca5c1b66056b9438d8866a522daeb3fee983b66970c0883')
-        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI_1
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI_2
 
     elif type_db == 'postgres':
-        
-        DATABASE_URI_1 = 'jdbc:postgresql://{host_name}:{port}/{database}?{sslmode}&{user}&{password}'\
-            .format(host_name='ec2-52-31-2-97.eu-west-1.compute.amazonaws.com', port='5432', database='db5veivij96r5u',\
-                    sslmode='require', user='fiysuzvofhprpp', password='ab6e8ad51efac658eca5c1b66056b9438d8866a522daeb3fee983b66970c0883')
-        DATABASE_URI_2 = 'postgresql://{user}:{password}@{host_name}:{port}/{database}'\
+        DATABASE_URI_HEROKU = 'postgresql://{user}:{password}@{host_name}:{port}/{database}'\
             .format( user='fiysuzvofhprpp', password='ab6e8ad51efac658eca5c1b66056b9438d8866a522daeb3fee983b66970c0883', \
                     host_name='ec2-52-31-2-97.eu-west-1.compute.amazonaws.com', port='5432', database='db5veivij96r5u',\
                     sslmode='require')
@@ -41,7 +37,7 @@ def connect_to_db_server(app,type_db=None):
         heroku = 'jdbc:postgresql://ec2-52-31-2-97.eu-west-1.compute.amazonaws.com:5432/db5veivij96r5u?user=fiysuzvofhprpp&password=ab6e8ad51efac658eca5c1b66056b9438d8866a522daeb3fee983b66970c0883'
         cloud_db = "postgres://fiysuzvofhprpp:ab6e8ad51efac658eca5c1b66056b9438d8866a522daeb3fee983b66970c0883@ec2-52-31-2-97.eu-west-1.compute.amazonaws.com:5432/db5veivij96r5u"
         
-        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI_2
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI_HEROKU
         
     else:
         app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test3.db"
