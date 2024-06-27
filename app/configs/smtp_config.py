030706a423c10071859ddd8cc56ef6ed79bf44d1
adolfosucumula <rocketmc2009@gmail.com>
2024-06-27 20:46:12 +0100

    testAuth

--- app/configs/smtp_config.py
+++ app/configs/smtp_config.py
@@ -4,10 +4,10 @@ import os
 def _get_smtp_config() -> tuple:
 
     # Get the SMTP configuration from the environment variables
-    smtp_host = os.environ.get('SMTP_HOST', 'smtp.gmail.com')
-    smtp_port = int(os.environ.get('SMTP_PORT', 465)) # 587 by default
-    smtp_user = os.environ.get('SMTP_USER', 'iledmd3@gmail.com')
-    smtp_password = os.environ.get('SMTP_PASSWORD', 'aecnrbosvgdvahzw')
+    smtp_host = os.environ.get('SMTP_HOST')
+    smtp_port = int(os.environ.get('SMTP_PORT')) # 587 by default
+    smtp_user = os.environ.get('SMTP_USER')
+    smtp_password = os.environ.get('SMTP_PASSWORD')
 
     # Yandex configurations
     """smtp_host = os.environ.get('SMTP_HOST', 'smtp.yandex.com')
