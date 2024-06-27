36e5f56eb33d95a8ee9b7b7f206e498e119836c5
adolfosucumula <rocketmc2009@gmail.com>
2024-06-27 20:46:43 +0100

    testAuth

--- app/configs/smtp_config.py
+++ app/configs/smtp_config.py
@@ -12,7 +12,7 @@ def _get_smtp_config() -> tuple:
     # Yandex configurations
     """smtp_host = os.environ.get('SMTP_HOST', 'smtp.yandex.com')
     smtp_port = int(os.environ.get('SMTP_PORT', 465)) # 465 for SSL
-    smtp_user = os.environ.get('SMTP_USER', 'data-tuning@yandex.com')
-    smtp_password = os.environ.get('SMTP_PASSWORD', 'aecnrbosvgdvahzw')
+    smtp_user = os.environ.get('SMTP_USER', '')
+    smtp_password = os.environ.get('SMTP_PASSWORD', '')
 """
     return smtp_host, smtp_port, smtp_user, smtp_password
