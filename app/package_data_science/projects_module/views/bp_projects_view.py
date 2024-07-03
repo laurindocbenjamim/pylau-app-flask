d498a26fdff0e0f33860299d5bbf837d85dd0477
adolfosucumula <rocketmc2009@gmail.com>
2024-07-03 21:33:58 +0100

    projects view

--- app/package_data_science/projects_module/views/bp_projects_view.py
+++ app/package_data_science/projects_module/views/bp_projects_view.py
@@ -6,9 +6,9 @@ from ....token_module.userTokenModel import UserToken
 from ....auth_package.module_sign_up_sub.views.removeUserAccountView import RemoveUserAccountView
 from ....auth_package.module_sign_up_sub.model.users import Users
 bp_project_view = Blueprint('projects', __name__, url_prefix='/projects', template_folder='templates')
-bp_project_view.add_url_rule('/list/<string:user_token>', view_func=ProjectsView.as_view('list',Users, UserToken, template='projects.html'))
+bp_project_view.add_url_rule('/list/<string:user_token>', view_func=ProjectsView.as_view('list',Users, UserToken, template='projects_test.html'))
 bp_project_view.add_url_rule('/user/remove/<int:user_id>/<string:user_token>', view_func=RemoveUserAccountView.as_view('user-remove',Users, UserToken))
 
 @bp_project_view.route('/')
 def projects():
-    return render_template('project.html')
+    return render_template('projects.html')
