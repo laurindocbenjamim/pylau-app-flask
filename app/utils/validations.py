ea1064972b8f8db15dcfe42c2eb7150a9ffc0f04
adolfosucumula <rocketmc2009@gmail.com>
2024-09-08 03:03:34 +0100

    troubleshooting enroll

--- app/utils/validations.py
+++ app/utils/validations.py
@@ -4,6 +4,7 @@ from flask import flash, Request
 
 def validate_str_and_punct_char():
     return True
+
 def validate_only_str(s):
     # This pattern allows spaces, accentuated characters, and common punctuation
     pattern = r'^[A-Za-zÀ-ÖØ-öø-ÿ\s\'"()]+$'
@@ -11,7 +12,7 @@ def validate_only_str(s):
         return True
     return False
 
-def lidate_str_punct_and_digits(s):
+def validate_str_punct_and_digits(s):
     # This pattern allows spaces, accentuated characters, numbers, and common punctuation
     pattern = r'^[A-Za-zÀ-ÖØ-öø-ÿ0-9\s.,;!?\'"()]+$'
     if re.match(pattern, s):
