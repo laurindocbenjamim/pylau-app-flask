60d4b9c5aafff27edadfd6892fd2e8863277f3d3
adolfosucumula <rocketmc2009@gmail.com>
2024-09-21 14:43:16 +0100

    create audit view

--- app/package_auditapp/bp_auditapp.py
+++ app/package_auditapp/bp_auditapp.py
@@ -2,7 +2,7 @@
 from flask_cors import CORS, cross_origin
 from flask_login import logout_user
 from markupsafe import escape
-from flask import Blueprint, render_template, redirect, url_for, session, request, make_response
+from flask import Blueprint, render_template, redirect, url_for, session, request, make_response, Response
 
 from app.auth_package.module_sign_up_sub.model.users import Users
 from app.token_module import UserToken
@@ -18,10 +18,43 @@ def read_log_in_chunk(file_path, chunck_size=1024):
                 break
             yield chunck
 
+@bp_audit.route('/')
+@cross_origin(methods=['GET'])
+def audit():
+
+    if 'user_token' in session:
+        user_token = session['user_token']
+        if len(escape(user_token)) < 100 or len(escape(user_token)) > 200:
+            session.clear()
+            logout_user()
+            return redirect(url_for('auth.user.login'))            
+            
+        if UserToken.is_user_token_expired(user_token):
+            session.clear()
+            logout_user()
+            return redirect(url_for('auth.user.login'))
+        
+        if request.method == 'GET' and user_token is not None:
+
+            status,token = UserToken.get_token_by_token(user_token)
+                
+            # Get the user details using the email address
+            if status and Users.check_email_exists(token.username):
+                session['user_token'] = token.token
+
+    resp = make_response(render_template('home.html', title='Audit App', log='audit/logs'))
+    return resp
+    
 
 @bp_audit.route('/logs')
 @cross_origin(methods=['GET'])
 def display_logs():
+
+    # Generate logs chunck
+    def generate():
+        file_path = "app/static/logs/logs.log"
+        for chunck in read_log_in_chunk(file_path):
+            yield chunck
 
     if 'user_token' in session:
         user_token = session['user_token']
@@ -45,10 +78,7 @@ def display_logs():
 
     #return jsonify({"user": session['user_token']})
     logs = "app/static/logs/logs.log"
-    log_data =''
-    """with open(logs, 'r') as data:
-        log_data = data.read()"""
-    log_data = read_log_in_chunk(logs, chunck_size=1024)
-    resp = make_response(render_template('home.html', title='Display Logs', log_data=log_data))
-    return resp
+    
+
+    return Response(generate(), mimetype='text/plain')
     
