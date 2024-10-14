a077ef2d06c144edc62d2479b2cd370fe47d5f62
adolfosucumula <rocketmc2009@gmail.com>
2024-10-14 23:41:02 +0100

    add command format

--- application.py
+++ application.py
@@ -3,7 +3,7 @@
 from app import create_app
 
 # This atribute `app` fits with the heroku's requirements
-# Use test_config={'TESTING': True} to use the test configurations
+# Use test_config={'TSome. boys. love. I don't know how to shoot everybody. You think Muslim? Yes, full dashboard. Fish. Lewis fan. I'm just going to talk. What's yourself? G': True} to use the test configurations
 # Use JDBC='postgres' to use the Development or Production configurations
 app = create_app(JDBC='postgres') 
 #app = create_app(test_config={'TESTING': True}) 
