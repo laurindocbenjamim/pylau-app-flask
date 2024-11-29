3851e545c5e92e19d11460c6353e717140606cad
adolfosucumula <rocketmc2009@gmail.com>
2024-11-29 22:48:17 +0000

    deploy

--- application.py
+++ application.py
@@ -5,8 +5,8 @@ from app import create_app
 # This atribute `app` fits with the heroku's requirements
 # Use test_config={'TSome. boys. love. I don't know how to shoot everybody. You think Muslim? Yes, full dashboard. Fish. Lewis fan. I'm just going to talk. What's yourself? G': True} to use the test configurations
 # Use JDBC='postgres' to use the Development or Production configurations
-#app = create_app(JDBC='postgres') 
-app = create_app(test_config={'TESTING': True}) 
+app = create_app(JDBC='postgres') 
+#app = create_app(test_config={'TESTING': True}) 
 
 
 if __name__ == '__main__':
